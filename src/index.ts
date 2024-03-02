f0f5892ef7c786d481a771874d851819718dafa4
Andriy Skachko <skachko.andrii@gmail.com>
2024-03-02 07:37:28 +0200

    dev(setup): eslint & prettier

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import dotenv from "dotenv";
-import express, { Request, Response } from "express";
+import dotenv from 'dotenv'
+import express, { Request, Response } from 'express'
 
-dotenv.config();
+dotenv.config()
 
-const app = express();
-const port = process.env.PORT || 3000;
+const app = express()
+const port = process.env.PORT || 3000
 
-app.get("/", (_: Request, res: Response) => {
-  res.send("Hello, World!");
-});
+app.get('/', (_: Request, res: Response) => {
+    res.send('Hello, World!')
+})
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+    console.log(`[server]: Server is running at http://localhost:${port}`)
+})
